23d1298e60deb51b08102a5b763b83909e2381e0
waltersjac <waltersjac@urbandaleschools.com>
2021-04-05 09:48:59 -0500

    1st test commit

--- 13.1_User_Control_Project.py
+++ 13.1_User_Control_Project.py
@@ -24,7 +24,7 @@ point_list=((100,90),(120,90),(120,200),(60,200),(60,340),(270,340),(270,400),(2
 point_list2=((450,0),(450,60),(430,60),(350,60),(350,360),(330,360),(330,280),(120,280),(120,260),(200,260),(200,140)
              ,(280,140),(280,160),(220,160),(220,260),(330,260),(330,40),(430,40),(430,0))
 
-class Ball:
+class player:
     def __init__(self,x,y,dx,dy,r,c):
         self.x=x
         self.y=y
@@ -33,10 +33,10 @@ class Ball:
         self.r=r
         self.c=c
         self.laser_sound = arcade.load_sound("explosion.mp3")
-    def draw_ball(self):
+    def draw_player(self):
         arcade.draw_triangle_filled(self.x,self.y,self.x-10,self.y-20,self.x+10,self.y-20,self.c)
 
-    def update_ball(self):
+    def update_player(self):
         self.x+= self.dx
         self.y+= self.dy
 
@@ -58,38 +58,41 @@ class Ball:
             self.y = SH
             arcade.play_sound(self.laser_sound,5)
 
+        if self.x==480 and self.y==380:
+            print("You win")
+
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.WHITE)
-        self.ball = Ball(20,20,0,0,15,arcade.color.ARMY_GREEN)
+        self.player = player(20,30,0,0,15,arcade.color.ARMY_GREEN)
 
     def on_draw(self):
         arcade.start_render()
-        self.ball.draw_ball()
+        self.player.draw_player()
         arcade.draw_polygon_filled(point_list,arcade.color.BLACK)
         arcade.draw_rectangle_filled(430,250,20,300,arcade.color.BLACK)
         arcade.draw_polygon_filled(point_list2,arcade.color.BLACK)
         arcade.draw_rectangle_filled(190,40,20,80,arcade.color.BLACK)
 
     def on_update(self, dt):
-        self.ball.update_ball()
+        self.player.update_player()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
-            self.ball.dx=-4
+            self.player.dx=-4
         elif key ==arcade.key.RIGHT:
-            self.ball.dx=4
+            self.player.dx=4
         elif key == arcade.key.UP:
-            self.ball.dy=4
+            self.player.dy=4
         elif key == arcade.key.DOWN:
-            self.ball.dy=-4
+            self.player.dy=-4
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.ball.dx = 0
+            self.player.dx = 0
         elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.ball.dy = 0
+            self.player.dy = 0
 
 
 def main():
